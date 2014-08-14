2ab162de87b4b0e2e964a8b93228794c0c29b69a
M. Ahmad Zafar <ahmadzafar@gmail.com>
2014-08-14 12:42:49 +0500

    Now displaying the photo also.

--- js/contacts.js
+++ js/contacts.js
@@ -7,12 +7,22 @@ $(document).ready(function() {
 		if(cursor.result) {
 			//take the contact name as a heading
 			var contact = "<div><h3>" + cursor.result.name + "</h3>";
+			console.log( cursor.result.photo );
+			
+			
 			
 			//loop through all the properties of the contact
 			$.each(cursor.result, function(key, value) {
 				
+				//if a picture is provided, treat it differently
+				if( key == 'photo' && value != null ) {
+					//large contact photo create from blob object
+					contact += "<div><img src='" + window.URL.createObjectURL(value[0]) + "' /></div>";
+					//small contact photo create from blob object
+					contact += "<div><img src='" + window.URL.createObjectURL(value[1]) + "' /></div>";
+				}
 				//make sure the property is not set to null not it matches any of the skipped list
-				if( value != null && $.inArray( key, ["init", "toJSON"] ) < 0 ) {
+				else if( value != null && $.inArray( key, ["init", "toJSON"] ) < 0 ) {
 					
 					//for complex objects, list all of their childs
 					if( typeof(value) === 'object' && $.inArray( key, ["name", "givenName", "additionalName", "familyName"] ) < 0 ) {
