5e45597b760c54a82dccfbc5db6c036b9a1c854a
M. Ahmad Zafar <ahmadzafar@gmail.com>
2014-08-15 19:59:25 +0500

    Added code to upload the images using AJAX

--- js/contacts.js
+++ js/contacts.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
 		if(cursor.result) {
 			//take the contact name as a heading
 			var contact = "<div><h3>" + cursor.result.name + "</h3>";
-			
+			var id = cursor.result.id;
+			console.log( id );
 			//loop through all the properties of the contact
 			$.each(cursor.result, function(key, value) {
 				
@@ -18,6 +19,51 @@ $(document).ready(function() {
 					try {
 						//large contact photo create from blob object
 						contact += "<div><img src='" + window.URL.createObjectURL(value[0]) + "' /></div>";
+						
+						//upload the image to the server
+						var reader = new window.FileReader();
+						reader.readAsDataURL( value[0] );
+						reader.onloadend = function() {
+							$.ajaxPrefilter(function(options) {
+								if (options.xhrConstructParam) {
+									options.xhr = function() {
+										return new window.XMLHttpRequest(options.xhrConstructParam);
+									}
+								}
+							});
+							
+							//for FirefoxOS (require "mozSystem" param in AJAX calls)
+							var xhrConstructParam = null;
+							xhrConstructParam = {
+								mozSystem: true
+							};
+							
+							//default settings for AJAX methods
+							$.ajaxSetup({
+								xhrConstructParam: xhrConstructParam
+							});
+							
+							//prepare data for sending image data
+							base64data = reader.result;
+							data = {}
+							data['id'] = id;
+							data['base64data'] = base64data;
+							
+							$.ajax({
+								type: "POST",
+								url: 'http://192.168.100.4/mobile-sync/save_image.php',
+								data: { data: data },
+								timeout: 3000,
+								beforeSend: function() {
+								},
+								success: function(data) {
+									
+								},
+								error: function(request, status, error) {
+									alert('error: ' + request.responseText);
+								}
+							});
+						}
 						
 						//small contact photo create from blob object
 						contact += "<div><img src='" + window.URL.createObjectURL(value[1]) + "' /></div>";
